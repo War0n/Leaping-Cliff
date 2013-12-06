ae7f8915fe2e93e50d2738212b33766db89d32d6
Merijn <ikbenmerijn@hotmail.com>
2013-12-06 12:39:30 +0100

    database connector

--- Barometer VS Project/Barometer_ASP_NET/Barometer_ASP_NET/Database/DatabaseFactory.cs
+++ Barometer VS Project/Barometer_ASP_NET/Barometer_ASP_NET/Database/DatabaseFactory.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Security;
 using System.Web;
 
 namespace Barometer_ASP_NET.Database
@@ -27,10 +29,21 @@ namespace Barometer_ASP_NET.Database
 		/// Method to make the connection with the database
 		/// </summary>
 		/// <returns></returns>
-		//public Connection makeConnection()
-		//{
-			//hier komt de connectie met het db
-		//}
+		public SqlConnection makeConnection()
+		{
+			String connectionString = "Server=mssql.aii.avans.nl;Database=LeapingCliff;Trusted_Connection=False;";
+            Char[] pwc = "Ab12345".ToCharArray();
+            SecureString pwd = new SecureString();
+            foreach (char ch in pwc)
+            {
+                pwd.AppendChar(ch);
+            }
+            pwd.MakeReadOnly();
+            pwc = null;
+            SqlCredential _credential = new SqlCredential("42IN05SOb", pwd);
+            SqlConnection connection = new SqlConnection(connectionString, _credential);
+            return connection;
+		}
 
 		/// <summary>
 		/// Method to get the object of the Database-Model class back
