d43e9d3ab549dc47358b5ae1ea0c26de4b84f590
Joost <joostvandenbrandt@hotmail.com>
2013-12-12 01:01:11 +0100

    Admin Views klaar..
    Controllers nog vullen met functies

---
+++ Barometer VS Project/Barometer_ASP_NET/Barometer_ASP_NET/Controllers/GroupController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Barometer_ASP_NET.Controllers
+{
+    public class GroupController : Controller
+    {
+        //
+        // GET: /Group/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        //Add Function
+
+        //Remove Function
+
+        //AddStudent Function 
+
+        //RemoveStudent Function
+
+        //ListStudents
+    }
+}
---
+++ Barometer VS Project/Barometer_ASP_NET/Barometer_ASP_NET/Controllers/ProjectController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Barometer_ASP_NET.Controllers
+{
+    public class ProjectController : Controller
+    {
+        //
+        // GET: /Project/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Detail Function
+    }
+}
