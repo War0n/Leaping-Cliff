6b3e2fd9edc069a6723f6802fd68c0fa3e77b6b8
awr.lacle@student.avans.nl <awr.lacle@student.avans.nl>
2013-12-16 13:52:45 +0100

    LDAP connector met controller en extra packages (nog niet af)

---
+++ Barometer VS Project/Barometer_ASP_NET/Barometer_ASP_NET/Controllers/AuthController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Barometer_ASP_NET.Database;
+using DotNetOpenAuth.OAuth;
+using DotNetOpenAuth.Messaging;
+using System.IO;
+
+namespace Barometer_ASP_NET.Controllers
+{
+    public class AuthController : Controller
+    {
+        //
+        // GET: /Auth/
+        LDAPConnector connr = LDAPConnector.GetInstance();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult StartOAuth()
+        {
+
+            var serviceProvider = connr.GetServiceDescription();
+            var consumer = new WebConsumer(serviceProvider, connr.tokenManager);
+
+            // Url to redirect to TODO
+            
+            var authUrl = new Uri(Request.Url.Scheme + "://" + Request.Url.Authority + "/Home/OAuthCallBack");
+
+            // request access
+            consumer.Channel.Send(consumer.PrepareRequestUserAuthorization(authUrl, null, null));
+
+            // This will not get hit!
+            return null;
+        }
+
+        public ActionResult OAuthCallback()
+        {
+            // Process result from the service provider
+            var serviceProvider = connr.GetServiceDescription();
+            var consumer = new WebConsumer(serviceProvider, connr.tokenManager);
+            var accessTokenResponse = consumer.ProcessUserAuthorization();
+
+            // If we didn't have an access token response, this wasn't called by the service provider
+            if (accessTokenResponse == null)
+                return RedirectToAction("Index");
+
+            // Extract the access token
+            string accessToken = accessTokenResponse.AccessToken;
+
+            ViewBag.Token = accessToken;
+            ViewBag.Secret = connr.tokenManager.GetTokenSecret(accessToken);
+            return View();
+        }
+        /*
+        public ActionResult Test2()
+        {
+            // Process result from linked in
+            var LiServiceProvider = connr.GetServiceDescription();
+            var linkedIn = new WebConsumer(LiServiceProvider, connr.tokenManager);
+            var accessToken = connr.GetServiceDescription().AccessTokenEndpoint;
+
+            // Retrieve the user's profile information
+            var endpoint = new MessageReceivingEndpoint("http://api.linkedin.com/v1/people/~", HttpDeliveryMethods.GetRequest);
+            var request = linkedIn.PrepareAuthorizedRequest(endpoint, accessToken);
+            var response = request.GetResponse();
+            ViewBag.Result = (new StreamReader(response.GetResponseStream())).ReadToEnd();
+
+            return View();
+        }*/
+    }
+         
+}
--- Barometer VS Project/Barometer_ASP_NET/Barometer_ASP_NET/Database/LDAPConnector.cs
+++ Barometer VS Project/Barometer_ASP_NET/Barometer_ASP_NET/Database/LDAPConnector.cs
@@ -2,11 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using DotNetOpenAuth;
+using DotNetOpenAuth.OAuth;
+using DotNetOpenAuth.AspNet.Clients;
+using DotNetOpenAuth.Messaging;
+using DotNetOpenAuth.OAuth.ChannelElements;
+using DotNetOpenAuth.OpenId;
+using DotNetOpenAuth.Configuration;
+using DotNetOpenAuth.Xrds;
+using DotNetOpenAuth.OAuth.Messages;
+using DotNetOpenAuth.OpenId.Extensions.OAuth;
+using DotNetOpenAuth.AspNet;
+
 
 namespace Barometer_ASP_NET.Database
 {
 	public class LDAPConnector
 	{
-		//hier moeten we ff over inlezen
+         private static LDAPConnector instance;
+         public  InMemoryOAuthTokenManager tokenManager;       
+
+        public static LDAPConnector GetInstance()
+        {
+           
+            if (instance == null)
+                instance = new LDAPConnector("73d120c3cebc89b2a344ca8b7e434d8f30349c70", "0179ed703f5f15277cefef2ec0259646dc4e2d36");
+            return instance;
+            
+        }
+
+        //Handles the connection info
+        public   ServiceProviderDescription GetServiceDescription()
+        {
+             return new ServiceProviderDescription
+            {
+            RequestTokenEndpoint = new MessageReceivingEndpoint("https://publicapi.avans.nl/oauth/request_token", HttpDeliveryMethods.GetRequest | HttpDeliveryMethods.AuthorizationHeaderRequest),
+            UserAuthorizationEndpoint = new MessageReceivingEndpoint("https://publicapi.avans.nl/oauth/saml.php", HttpDeliveryMethods.GetRequest | HttpDeliveryMethods.AuthorizationHeaderRequest),
+            AccessTokenEndpoint = new MessageReceivingEndpoint("https://publicapi.avans.nl/oauth/access_token", HttpDeliveryMethods.GetRequest | HttpDeliveryMethods.AuthorizationHeaderRequest),
+            TamperProtectionElements = new ITamperProtectionChannelBindingElement[] { new HmacSha1SigningBindingElement() }
+            };
+        }
+
+        
+         public LDAPConnector(String consumerKey,String consumerSecret) 
+        {
+            tokenManager = new InMemoryOAuthTokenManager(consumerKey, consumerSecret);  
+             
+        }
+
+        
+        
+       
+        public DotNetOpenAuth.AspNet.AuthenticationResult VerifyAuthenticationCore(DotNetOpenAuth.OAuth.Messages.AuthorizedTokenResponse response)
+        {
+            //Perform the verification process
+            return new AuthenticationResult(true);
+        }
 	}
+
 }
