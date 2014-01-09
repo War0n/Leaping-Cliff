3827a0c3ce1345f545bd8c5db78c0d69ed390778
Pineapples <ms.celie@student.avans.nl>
2014-01-10 00:04:22 +0100

    Testclass voor DAOProject is compleet.

--- Barometer VS Project/Barometer_ASP_NET/Barometer_ASP_NET.Tests/Controllers/TEST_DAOProject.cs
+++ Barometer VS Project/Barometer_ASP_NET/Barometer_ASP_NET.Tests/Controllers/TEST_DAOProject.cs
@@ -567,8 +567,12 @@ namespace Barometer_ASP_NET.Tests.Controllers
             Assert.AreEqual("", exception);
             Assert.IsTrue(b.Count > 0);
 
-            exception = "";
-            
+        [TestMethod]
+        public void test_23_getTutors_valid_nonexistent_input()
+        {
+            String exception = "";
+            Dictionary<string, string> b = new Dictionary<string, string>();
+
             try
             {
                 b = d.getTutors(99999);
@@ -577,10 +581,7 @@ namespace Barometer_ASP_NET.Tests.Controllers
             {
                 exception = e.ToString();
             }
-            Assert.AreEqual("", exception);
-            Assert.IsTrue(b.Count == 0);
-
-
+            Assert.IsTrue(exception.Contains("DataException"));
         }
 
         [TestMethod]
@@ -634,7 +635,6 @@ namespace Barometer_ASP_NET.Tests.Controllers
 
             Assert.IsTrue(exception.Contains("DataException"));
         }
-
         [TestMethod]
         public void test_111_getCurrentActiveProject_negative_input()
         {
@@ -666,8 +666,7 @@ namespace Barometer_ASP_NET.Tests.Controllers
             {
                 exception = e.ToString();
             }
-            Assert.AreEqual("", exception);
-            Assert.IsTrue(b.Count == 0);
+            Assert.IsTrue(exception.Contains("DataException"));
         }
 
         [TestMethod]
@@ -685,6 +684,411 @@ namespace Barometer_ASP_NET.Tests.Controllers
 
             Assert.AreEqual("", exception);
             Assert.IsTrue(exception.Contains("ArgumentOutOfRangeException"));
+        }
+
+        [TestMethod]
+        public void test_113_getCurrentActiveProject_valid_nonexistent_input()
+        {
+            IQueryable<Project> test_collection;
+            String exception = "";
+            try
+            {
+                test_collection = d.GetCurrentActiveProject(9);
+            }
+            catch (Exception e)
+            {
+                exception = e.ToString();
+            }
+            catch (Exception e)
+            {
+                exception = e.ToString();
+            }
+
+            Assert.AreEqual("", exception);
+            Assert.AreNotEqual("", pName);
+        }
+
+        [TestMethod]
+        public void test_43_getProjectName_valid_nonexistent_input()
+        {
+            String exception = "";
+            String pName = "";
+
+            try
+            {
+                pName = d.getProjectName(9999999);
+            }
+            catch (Exception e)
+            {
+                exception = e.ToString();
+            }
+
+            Assert.IsTrue(exception.Contains("DataException"));
+        }
+
+        [TestMethod]
+        public void test_51_getStartAndEndDate_invalid_input()
+        {
+            String exception = "";
+            try
+            {
+                Dictionary<string, string> b = d.getStartAndEndDate(-1);
+            }
+            catch (Exception e)
+            {
+                exception = e.ToString();
+            }
+
+            Assert.IsTrue(exception.Contains("ArgumentOutOfRangeException"));
+        }
+
+        [TestMethod]
+        public void test_52_getStartAndEndDate_valid_input()
+        {
+            String exception = "";
+            Dictionary<string, string> b = new Dictionary<string, string>();
+            try
+            {
+                b = d.getStartAndEndDate(1);
+            }
+            catch (Exception e)
+            {
+                exception = e.ToString();
+            }
+            Assert.AreEqual("", exception);
+            Assert.IsTrue(b.Count > 0);
+        }
+
+        [TestMethod]
+        public void test_53_getStartAndEndDate_valid_nonexistent_input()
+        {
+            String exception = "";
+            Dictionary<string, string> b = new Dictionary<string, string>();
+            try
+            {
+                b = d.getStartAndEndDate(99999);
+            }
+            catch (Exception e)
+            {
+                exception = e.ToString();
+            }
+            Assert.IsTrue(exception.Contains("DataException"));
+        }
+
+        [TestMethod]
+        public void test_61_getProjectSummary_invalid_input()
+        {
+            String exception = "";
+            try
+            {
+                String pSummary = d.getProjectSummary(-1);
+            }
+            catch (Exception e)
+            {
+                exception = e.ToString();
+            }
+
+            Assert.IsTrue(exception.Contains("ArgumentOutOfRangeException"));
+        }
+
+        [TestMethod]
+        public void test_62_getProjectSummary_valid_input()
+        {
+            String exception = "";
+            String pSummary = "";
+            try
+            {
+                pSummary = d.getProjectSummary(1);
+            }
+            catch (Exception e)
+            {
+                exception = e.ToString();
+            }
+
+            Assert.AreEqual("", exception);
+            Assert.AreNotEqual("", pSummary);
+        }
+
+        [TestMethod]
+        public void test_63_getProjectSummary_valid_nonexistent_input()
+        {
+            String exception = "";
+            String pSummary = "";
+
+            try
+            {
+                pSummary = d.getProjectSummary(9999999);
+            }
+            catch (Exception e)
+            {
+                exception = e.ToString();
+            }
+
+            Assert.IsTrue(exception.Contains("DataException"));
+        }
+
+        [TestMethod]
+        public void test_71_getProjectTeachers_invalid_input()
+        {
+            String exception = "";
+            try
+            {
+                Dictionary<string, string> b = d.getProjectTeachers(-1);
+            }
+            catch (Exception e)
+            {
+                exception = e.ToString();
+            }
+
+            Assert.IsTrue(exception.Contains("ArgumentOutOfRangeException"));
+        }
+
+        [TestMethod]
+        public void test_72_getProjectTeachers_valid_input()
+        {
+            String exception = "";
+            Dictionary<string, string> b = new Dictionary<string, string>();
+            try
+            {
+                b = d.getProjectTeachers(1);
+            }
+            catch (Exception e)
+            {
+                exception = e.ToString();
+            }
+            Assert.AreEqual("", exception);
+            Assert.IsTrue(b.Count > 0);
+        }
+
+        [TestMethod]
+        public void test_73_getProjectTeachers_valid_nonexistent_input()
+        {
+            String exception = "";
+            Dictionary<string, string> b = new Dictionary<string, string>();
+            try
+            {
+                b = d.getProjectTeachers(99999);
+            }
+            catch (Exception e)
+            {
+                exception = e.ToString();
+            }
+            Assert.IsTrue(exception.Contains("DataException"));
+        }
+
+        [TestMethod]
+        public void test_81_getUsersInGroup_negative_input()
+        {
+            String s = "";
+            try
+            {
+                IQueryable<User> test_collection = d.getUsersInGroup(-1);
+            }
+            catch (Exception e)
+            {
+                s = e.ToString();
+            }
+
+            Assert.IsTrue(exception.Contains("DataException"));
+
+        }
+
+        [TestMethod]
+        public void test_82_getUsersInGroup_valid_input()
+        {
+            IQueryable<User> test_collection;
+            int i = 0;
+            String exception = "";
+
+            try
+            {
+                test_collection = d.getUsersInGroup(1);
+                foreach (User u in test_collection)
+                {
+                    i++;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Assert.AreEqual("", exception);
+            Assert.IsTrue(i > 0);
+        }
+
+        [TestMethod]
+        public void test_83_getUsersInGroup_valid_nonexistent_input()
+        {
+            IQueryable<User> test_collection;
+            String exception = "";
+            try
+            {
+                test_collection = d.getUsersInGroup(99924);
+            }
+            catch (Exception e)
+            {
+                exception = e.ToString();
+            }
+
+            Assert.IsTrue(exception.Contains("DataException"));
+        }
+
+        [TestMethod]
+        public void test_91_getProjectOwners_negative_input()
+        {
+            String s = "";
+            try
+            {
+                IQueryable<User> test_collection = d.GetProjectOwners(-1);
+            }
+            catch (Exception e)
+            {
+                s = e.ToString();
+            }
+
+            Assert.IsTrue(s.Contains("ArgumentOutOfRangeException"));
+
+        }
+
+        [TestMethod]
+        public void test_92_getProjectOwners_valid_input()
+        {
+            IQueryable<User> test_collection;
+            int i = 0;
+            String exception = "";
+
+            try
+            {
+                test_collection = d.GetProjectOwners(1);
+                foreach (User u in test_collection)
+                {
+                    i++;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Assert.AreEqual("", exception);
+            Assert.IsTrue(i > 0);
+        }
+
+        [TestMethod]
+        public void test_93_getProjectOwners_valid_nonexistent_input()
+        {
+            IQueryable<User> test_collection;
+            String exception = "";
+            try
+            {
+                test_collection = d.GetProjectOwners(99924);
+            }
+            catch (Exception e)
+            {
+                exception = e.ToString();
+            }
+
+            Assert.IsTrue(exception.Contains("DataException"));
+        }
+
+        [TestMethod]
+        public void test_101_getTutor_negative_input()
+        {
+            String s = "";
+            try
+            {
+                IQueryable<User> test_collection = d.GetTutor(-1);
+            }
+            catch (Exception e)
+            {
+                s = e.ToString();
+            }
+
+            Assert.IsTrue(s.Contains("ArgumentOutOfRangeException"));
+
+        }
+
+        [TestMethod]
+        public void test_102_getTutor_valid_input()
+        {
+            IQueryable<User> test_collection;
+            int i = 0;
+            String exception = "";
+
+            try
+            {
+                test_collection = d.GetTutor(1);
+                foreach (User u in test_collection)
+                {
+                    i++;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Assert.AreEqual("", exception);
+            Assert.IsTrue(i > 0);
+        }
+
+        [TestMethod]
+        public void test_103_getTutor_valid_nonexistent_input()
+        {
+            IQueryable<User> test_collection;
+            String exception = "";
+            try
+            {
+                test_collection = d.GetTutor(99924);
+            }
+            catch (Exception e)
+            {
+                exception = e.ToString();
+            }
+
+            Assert.IsTrue(exception.Contains("DataException"));
+        }
+
+        [TestMethod]
+        public void test_111_getCurrentActiveProject_negative_input()
+        {
+            String s = "";
+            try
+            {
+                IQueryable<Project> test_collection = d.GetCurrentActiveProject(-1);
+            }
+            catch (Exception e)
+            {
+                s = e.ToString();
+            }
+
+            Assert.IsTrue(s.Contains("ArgumentOutOfRangeException"));
+
+        }
+
+        [TestMethod]
+        public void test_112_getCurrentActiveProject_valid_input()
+        {
+            IQueryable<Project> test_collection;
+            int i = 0;
+            String exception = "";
+
+            try
+            {
+                test_collection = d.GetCurrentActiveProject(3000000);
+                foreach (Project u in test_collection)
+                {
+                    i++;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Assert.AreEqual("", exception);
+            Assert.IsTrue(i > 0);
         }
 
         [TestMethod]
