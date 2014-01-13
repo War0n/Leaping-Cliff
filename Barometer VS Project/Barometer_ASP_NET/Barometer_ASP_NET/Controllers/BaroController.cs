c3eaf5409f234fe2f8d730fdea48b98ad4960fec
Hoevenaars Jens <jens_h@live.nl>
2014-01-13 12:01:26 +0100

    Baro widget laatste bugs gefixeerd en commentaar erbij gezet

--- Barometer VS Project/Barometer_ASP_NET/Barometer_ASP_NET/Controllers/BaroController.cs
+++ Barometer VS Project/Barometer_ASP_NET/Barometer_ASP_NET/Controllers/BaroController.cs
@@ -12,15 +12,24 @@ namespace Barometer_ASP_NET.Controllers
 {
     public class BaroController : Controller
     {
+        /// <summary>
+        /// Database
+        /// </summary>
         private DatabaseClassesDataContext _db = DatabaseFactory.getInstance().getDataContext();
 
-        public ActionResult Index()
-        {
-            return View();
-        }
-
+        /// <summary>
+        /// Toon een report card. Wordt alleen aangeroepen vanaf de Holder.
+        /// </summary>
+        /// <param name="reporter_id">Reporter</param>
+        /// <param name="subject_id">Subject</param>
+        /// <param name="project_id">Project</param>
+        /// <param name="project_report_date_id">Report date</param>
+        /// <returns></returns>
         public ActionResult Widget(int reporter_id = 0, int subject_id = 0, int project_id = 0, int project_report_date_id = 0)
         {
+            if (!Request.IsAjaxRequest())
+                return HttpNotFound();
+
             BaroWidgetWrapper viewModel = (from p in _db.Projects
                           where (p.id == project_id)
                           select p).Select(r => new BaroWidgetWrapper{
@@ -36,8 +45,23 @@ namespace Barometer_ASP_NET.Controllers
             return PartialView("_BaroWidget", viewModel);
         }
 
+
+        /// <summary>
+        /// Haal de holder op voor het bekijken van de beoordelingen.
+        /// Deze weergeeft de SELECT met daarin de report dates. 
+        /// Bij het selecteren van de report date wordt er een request gemaakt naar de Widget()
+        /// </summary>
+        /// <param name="reporter_id"></param>
+        /// <param name="subject_id"></param>
+        /// <param name="project_id"></param>
+        /// <returns></returns>
         public ActionResult WidgetHolder(int reporter_id = 0, int subject_id = 0, int project_id = 0)
         {
+            if (!Request.IsAjaxRequest())
+                return HttpNotFound();
+
+
+            // Maak de viewmodel aan
             BaroWidgetWrapper viewModel = (from p in _db.Projects
                                            where (p.id == project_id)
                                            select p).Select(r => new BaroWidgetWrapper
