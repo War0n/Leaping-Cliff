20afe44ef5b7dc87674cf98bd7917f00cb6bd5c8
Pineapples <ms.celie@student.avans.nl>
2014-01-13 16:43:39 +0100

    Moq integration

--- Barometer VS Project/Barometer_ASP_NET/Barometer_ASP_NET.Tests/TEST_Controllers/TEST_AdminController.cs
+++ Barometer VS Project/Barometer_ASP_NET/Barometer_ASP_NET.Tests/TEST_Controllers/TEST_AdminController.cs
@@ -3,6 +3,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Barometer_ASP_NET.Controllers;
 using BarometerDataAccesLayer;
 using System.Web.Mvc;
+using Moq;
+using System.Web;
+using System.Web.Routing;
+using System.IO;
+using System.Configuration;
+using BarometerDataAccesLayer.Database;
 
 namespace Barometer_ASP_NET.Tests.TEST_Controllers
 {
@@ -10,6 +16,15 @@ namespace Barometer_ASP_NET.Tests.TEST_Controllers
     public class TEST_AdminController
     {
         AdminController a = new AdminController();
+        private FileStream _stream;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _stream = new FileStream("../../../Barometer_ASP_NET/Content/ExcelTemplates/GradeTemplate.xlsx",
+                         FileMode.Open);
+
+        }
 
         [TestMethod]
         public void test_01_student_correct_testdata()
@@ -18,6 +33,85 @@ namespace Barometer_ASP_NET.Tests.TEST_Controllers
             var resultdata = (User)result.ViewData.Model;
 
             Assert.AreEqual("John ", resultdata.firstname);
+        }
+
+        [TestMethod]
+        public void test_02_studentForm()
+        {
+            var controller = new AdminController();
+            FormCollection a = new FormCollection();
+            a.Add("student", "3000000");
+            var result = (RedirectToRouteResult) controller.StudentForm(a);
+            Assert.AreEqual("Student", result.RouteValues["action"]);
+        }
+
+        //[TestMethod]
+        public void test_03_create_and_delete_project() 
+        {
+            var controller = new AdminController();
+
+            var server = new Mock<HttpServerUtilityBase>();
+            var response = new Mock<HttpResponseBase>();
+
+            var request = new Mock<HttpRequestBase>();
+            request.Setup(r => r.UserHostAddress).Returns("localhost");
+
+            var session = new Mock<HttpSessionStateBase>();
+            session.Setup(s => s.SessionID).Returns(Guid.NewGuid().ToString());
+
+            var file = new Mock<HttpPostedFileBase>();
+            file.Setup(x => x.InputStream).Returns(_stream);
+            file.Setup(x => x.ContentLength).Returns((int)_stream.Length);
+            file.Setup(x => x.FileName).Returns(_stream.Name);
+
+            var files = new Mock<HttpFileCollectionBase>();
+            files.Setup(r => r.Count).Returns(1);
+            files.Setup(r => r.Get(0).InputStream).Returns(file.Object.InputStream);
+            request.Setup(r => r.Files).Returns(files.Object);
+            //request.Setup(r => r.Files[0]).Returns(file.Object);
+
+            var context = new Mock<HttpContextBase>();
+            context.Setup(c => c.Request).Returns(request.Object);
+            context.SetupGet(c => c.Request).Returns(request.Object);
+            context.SetupGet(c => c.Response).Returns(response.Object);
+            context.SetupGet(c => c.Server).Returns(server.Object);
+            context.SetupGet(c => c.Session).Returns(session.Object);
+
+
+            controller.ControllerContext = new ControllerContext(context.Object,
+                                              new RouteData(), controller);
+
+            CurrentUser currentUser = CurrentUser.getInstance();
+            currentUser.Studentnummer = 3000000;
+
+            FormCollection n = new FormCollection();
+            string exception = "";
+            n.Add("FormProject.name", "TestProject");
+            n.Add("FormProject.description", "TestDescription");
+            n.Add("FormProject.start_date", "01/01/2001");
+            n.Add("FormProject.end_date", "12/12/2012");
+            try
+            {
+                var result = (RedirectToRouteResult) controller.Create(n);
+                Assert.AreEqual("List", result.RouteValues["action"]);
+            }
+            catch (Exception e)
+            {
+                exception = e.ToString();
+            }
+            Assert.AreEqual(exception, "");
+
+            exception = "";
+
+            try{
+                a.DeleteProject(8);
+            }
+            catch (Exception e)
+            {
+                exception = e.ToString();
+            }
+
+            Assert.AreEqual(exception, "");
         }
     }
 }
--- Barometer VS Project/Barometer_ASP_NET/Barometer_ASP_NET.Tests/TEST_FileFactory/TEST_StudentExcel.cs
+++ Barometer VS Project/Barometer_ASP_NET/Barometer_ASP_NET.Tests/TEST_FileFactory/TEST_StudentExcel.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Barometer_ASP_NET.FileFactory;
+using Moq;
+using System.Web;
+using System.Web.Routing;
+using System.Web.Mvc;
 
 namespace Barometer_ASP_NET.Tests.TEST_FileFactory
 {
@@ -9,11 +13,29 @@ namespace Barometer_ASP_NET.Tests.TEST_FileFactory
     {
         String path = "";
 
-        //obsolete
         //[TestMethod]
         //public void test_01_export_studentexcel()
         //{
-        //    StudentExcel s = new StudentExcel();
+        //    var controller = new StudentExcel();
+
+        //    var server = new Mock<HttpServerUtilityBase>(MockBehavior.Loose);
+        //    var response = new Mock<HttpResponseBase>(MockBehavior.Strict);
+
+        //    var request = new Mock<HttpRequestBase>(MockBehavior.Strict);
+        //    request.Setup(r => r.UserHostAddress).Returns("localhost");
+
+        //    var session = new Mock<HttpSessionStateBase>();
+        //    session.Setup(s => s.SessionID).Returns(Guid.NewGuid().ToString());
+
+        //    var context = new Mock<HttpContextBase>();
+        //    context.SetupGet(c => c.Request).Returns(request.Object);
+        //    context.SetupGet(c => c.Response).Returns(response.Object);
+        //    context.SetupGet(c => c.Server).Returns(server.Object);
+        //    context.SetupGet(c => c.Session).Returns(session.Object);
+
+        //    controller.ControllerContext = new ControllerContext(context.Object,
+        //                                      new RouteData(), controller);
+
         //    int[] students = new int[1] { 3000000 };
         //    try
         //    {
