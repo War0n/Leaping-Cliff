583f366a8571db8bac3952b2f3290582d7bf08aa
Pineapples <ms.celie@student.avans.nl>
2014-01-19 12:57:39 +0100

    fix #4 added error handling

---
+++ Barometer VS Project/Barometer_ASP_NET/Barometer_ASP_NET/Controllers/ErrorController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Barometer_ASP_NET.Controllers
+{
+    public class ErrorController : Controller
+    {
+
+        // GET: /Error/HttpError404
+        public ActionResult HttpError404(string message)
+        {
+            return PartialView("HttpError404", message);
+        }
+
+        // GET: /Error/HttpError500
+        public ActionResult HttpError500(string message)
+        {
+            return PartialView("HttpError500", message);
+        }
+
+        // GET: /Error/General
+        public ActionResult General(string message)
+        {
+            return PartialView("General", message);
+        }
+
+    }
+}
