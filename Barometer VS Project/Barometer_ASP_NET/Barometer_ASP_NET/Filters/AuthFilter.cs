b41bf150a89f3feb459bd2e992ecce47de113780
awr.lacle@student.avans.nl <awr.lacle@student.avans.nl>
2014-01-21 15:02:51 +0100

    filter getest en werkt

--- Barometer VS Project/Barometer_ASP_NET/Barometer_ASP_NET/Filters/AuthFilter.cs
+++ Barometer VS Project/Barometer_ASP_NET/Barometer_ASP_NET/Filters/AuthFilter.cs
@@ -23,7 +23,7 @@ namespace Barometer_ASP_NET.Filters
             if(curUser.Role == null) {
                 filterContext.Result = new RedirectResult("/Account/login");
             }
-            else if (!curUser.Role.ToLower().Equals(GivenRole.ToLower()) && !GivenRole.ToLower().Equals("all") && !GivenRole.ToLower().Equals("admod"))
+            if (!curUser.Role.ToLower().Equals(GivenRole.ToLower()) && !GivenRole.ToLower().Equals("all") && !GivenRole.ToLower().Equals("admod"))
                 {                    
                         filterContext.Result = new RedirectResult("/Account/login");                                     
                 }
